dcd8fce7a8c2a9cae7253f87dfdd5097f7df4022
Skyler <SkyBlue43@users.noreply.github.com>
2025-10-02 20:50:38 -0600

    Fixed main test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -1,4 +1,6 @@
+import { Page } from "@playwright/test";
 import { test, expect } from "playwright-test-coverage";
+import { Role, User } from "../src/service/pizzaService";
 
 test("Home page loads", async ({ page }) => {
   await page.goto("http://localhost:5173/");
@@ -7,7 +9,42 @@ test("Home page loads", async ({ page }) => {
   await expect(page).toHaveTitle("JWT Pizza");
 });
 
-test("purchase with login", async ({ page }) => {
+async function basicInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = {
+    "d@jwt.com": {
+      id: "3",
+      name: "Kai Chen",
+      email: "d@jwt.com",
+      password: "a",
+      roles: [{ role: Role.Diner }],
+    },
+  };
+
+  // Authorize login for the given user
+  await page.route("*/**/api/auth", async (route) => {
+    const loginReq = route.request().postDataJSON();
+    const user = validUsers[loginReq.email];
+    if (!user || user.password !== loginReq.password) {
+      await route.fulfill({ status: 401, json: { error: "Unauthorized" } });
+      return;
+    }
+    loggedInUser = validUsers[loginReq.email];
+    const loginRes = {
+      user: loggedInUser,
+      token: "abcdef",
+    };
+    expect(route.request().method()).toBe("PUT");
+    await route.fulfill({ json: loginRes });
+  });
+
+  // Return the currently logged in user
+  await page.route("*/**/api/user/me", async (route) => {
+    expect(route.request().method()).toBe("GET");
+    await route.fulfill({ json: loggedInUser });
+  });
+
+  // A standard menu
   await page.route("*/**/api/order/menu", async (route) => {
     const menuRes = [
       {
@@ -29,74 +66,60 @@ test("purchase with login", async ({ page }) => {
     await route.fulfill({ json: menuRes });
   });
 
-  await page.route("*/**/api/franchise", async (route) => {
-    const franchiseRes = [
-      {
-        id: 2,
-        name: "LotaPizza",
-        stores: [
-          { id: 4, name: "Lehi" },
-          { id: 5, name: "Springville" },
-          { id: 6, name: "American Fork" },
-        ],
-      },
-      { id: 3, name: "PizzaCorp", stores: [{ id: 7, name: "Spanish Fork" }] },
-      { id: 4, name: "topSpot", stores: [] },
-    ];
+  // Standard franchises and stores
+  await page.route(/\/api\/franchise(\?.*)?$/, async (route) => {
+    const franchiseRes = {
+      franchises: [
+        {
+          id: 2,
+          name: "LotaPizza",
+          stores: [
+            { id: 4, name: "Lehi" },
+            { id: 5, name: "Springville" },
+            { id: 6, name: "American Fork" },
+          ],
+        },
+        { id: 3, name: "PizzaCorp", stores: [{ id: 7, name: "Spanish Fork" }] },
+        { id: 4, name: "topSpot", stores: [] },
+      ],
+    };
     expect(route.request().method()).toBe("GET");
     await route.fulfill({ json: franchiseRes });
   });
 
-  await page.route("*/**/api/auth", async (route) => {
-    const loginReq = { email: "d@jwt.com", password: "a" };
-    const loginRes = {
-      user: {
-        id: 3,
-        name: "Kai Chen",
-        email: "d@jwt.com",
-        roles: [{ role: "diner" }],
-      },
-      token: "abcdef",
-    };
-    expect(route.request().method()).toBe("PUT");
-    expect(route.request().postDataJSON()).toMatchObject(loginReq);
-    await route.fulfill({ json: loginRes });
-  });
-
+  // Order a pizza.
   await page.route("*/**/api/order", async (route) => {
-    const orderReq = {
-      items: [
-        { menuId: 1, description: "Veggie", price: 0.0038 },
-        { menuId: 2, description: "Pepperoni", price: 0.0042 },
-      ],
-      storeId: "4",
-      franchiseId: 2,
-    };
+    const orderReq = route.request().postDataJSON();
     const orderRes = {
-      order: {
-        items: [
-          { menuId: 1, description: "Veggie", price: 0.0038 },
-          { menuId: 2, description: "Pepperoni", price: 0.0042 },
-        ],
-        storeId: "4",
-        franchiseId: 2,
-        id: 23,
-      },
+      order: { ...orderReq, id: 23 },
       jwt: "eyJpYXQ",
     };
     expect(route.request().method()).toBe("POST");
-    expect(route.request().postDataJSON()).toMatchObject(orderReq);
     await route.fulfill({ json: orderRes });
   });
 
   await page.goto("/");
+}
+
+test("login", async ({ page }) => {
+  await basicInit(page);
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("d@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("a");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("link", { name: "KC" })).toBeVisible();
+});
+
+test("purchase with login", async ({ page }) => {
+  await basicInit(page);
 
   // Go to order page
   await page.getByRole("button", { name: "Order now" }).click();
 
   // Create order
   await expect(page.locator("h2")).toContainText("Awesome is a click away");
-  await page.getByRole("combobox").selectOption("1");
+  await page.getByRole("combobox").selectOption("4");
   await page.getByRole("link", { name: "Image Description Veggie A" }).click();
   await page.getByRole("link", { name: "Image Description Pepperoni" }).click();
   await expect(page.locator("form")).toContainText("Selected pizzas: 2");
@@ -110,14 +133,14 @@ test("purchase with login", async ({ page }) => {
   await page.getByRole("button", { name: "Login" }).click();
 
   // Pay
-  // await expect(page.getByRole("main")).toContainText(
-  //   "Send me those 2 pizzas right now!"
-  // );
-  // await expect(page.locator("tbody")).toContainText("Veggie");
-  // await expect(page.locator("tbody")).toContainText("Pepperoni");
-  // await expect(page.locator("tfoot")).toContainText("0.008 ₿");
-  // await page.getByRole("button", { name: "Pay now" }).click();
+  await expect(page.getByRole("main")).toContainText(
+    "Send me those 2 pizzas right now!"
+  );
+  await expect(page.locator("tbody")).toContainText("Veggie");
+  await expect(page.locator("tbody")).toContainText("Pepperoni");
+  await expect(page.locator("tfoot")).toContainText("0.008 ₿");
+  await page.getByRole("button", { name: "Pay now" }).click();
 
-  // // Check balance
-  // await expect(page.getByText("0.008")).toBeVisible();
+  // Check balance
+  await expect(page.getByText("0.008")).toBeVisible();
 });
---
+++ tests/register.spec.ts
@@ -0,0 +1,27 @@
+import { Page } from "@playwright/test";
+import { test, expect } from "playwright-test-coverage";
+import { Role, User } from "../src/service/pizzaService";
+
+test("Home page loads", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("JWT Pizza");
+});
+
+async function basicInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = {
+    "d@jwt.com": {
+      id: "3",
+      name: "Kai Chen",
+      email: "d@jwt.com",
+      password: "a",
+      roles: [{ role: Role.Diner }],
+    },
+  };
+
+  await page.goto("/");
+}
+
+test("register", async () => {});
