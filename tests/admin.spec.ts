02030a928c0e8cad111d12e3145a53c7efa2e393
Skyler <SkyBlue43@users.noreply.github.com>
2025-10-02 23:04:02 -0600

    80 percent coverage with backend running

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -13,4 +13,66 @@ async function basicInit(page: Page) {
   await page.goto("/");
 }
 
-test("Admin login", async () => {});
+test("Create and delete franchise", async ({ page }) => {
+  await basicInit(page);
+  await page
+    .getByLabel("Global")
+    .getByRole("link", { name: "Franchise" })
+    .click();
+  await page.getByRole("link", { name: "login", exact: true }).click();
+  await expect(page.getByText("Welcome back")).toBeVisible();
+  await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByRole("link", { name: "常" })).toBeVisible();
+  await page.getByRole("link", { name: "Admin" }).click();
+  await expect(page.getByText("Mama Ricci's kitchen")).toBeVisible();
+  await page.getByRole("button", { name: "Add Franchise" }).click();
+  await page.getByRole("textbox", { name: "franchise name" }).fill("test");
+  await page
+    .getByRole("textbox", { name: "franchisee admin email" })
+    .fill("a@jwt.com");
+  await expect(
+    page.getByText("Create franchise", { exact: true })
+  ).toBeVisible();
+  await page.getByRole("button", { name: "Create" }).click();
+  await expect(page.getByRole("cell", { name: "常用名字" })).toBeVisible();
+  await page
+    .getByRole("row", { name: "test 常用名字 Close" })
+    .getByRole("button")
+    .click();
+  await expect(page.getByText("Sorry to see you go")).toBeVisible();
+  await page.getByRole("button", { name: "Close" }).click();
+});
+
+test("Create and delete store", async ({ page }) => {
+  await basicInit(page);
+
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("f@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("franchisee");
+  await page.getByRole("textbox", { name: "Password" }).press("Enter");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page
+    .getByLabel("Global")
+    .getByRole("link", { name: "Franchise" })
+    .click();
+
+  await page
+    .getByLabel("Global")
+    .getByRole("link", { name: "Franchise" })
+    .click();
+  await expect(page.getByText("pizzaPocket")).toBeVisible();
+  await page.getByRole("button", { name: "Create store" }).click();
+  await page.getByRole("textbox", { name: "store name" }).click();
+  await expect(page.getByText("Create store")).toBeVisible();
+  await page.getByRole("textbox", { name: "store name" }).fill("test");
+  await page.getByRole("button", { name: "Create" }).click();
+  await expect(page.getByRole("cell", { name: "test" })).toBeVisible();
+  await page
+    .getByRole("row", { name: "test 0 ₿ Close" })
+    .getByRole("button")
+    .click();
+  await expect(page.getByText("Sorry to see you go")).toBeVisible();
+  await page.getByRole("button", { name: "Close" }).click();
+});
